cc4f8e82558f1b2ce7d41a9c2317431b75003ed3
Andela_Vanesa <33512792+AndelaVanesa@users.noreply.github.com>
2018-02-19 18:10:29 +0100

    Add files via upload

---
+++ Osnove programiranja/filip/prvi.py
@@ -0,0 +1,7 @@
+from math import sqrt
+
+a, b, c = 3, 4, 1
+
+D = sqrt(a**2 + b**2 + c**2)
+
+print('Duljina prostorne dijagonale sa stranicama duljine {}, {} i {} cm je {:.3f} cm.'.format(a,b,c,D))
