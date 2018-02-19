f0a5f891f901366bd664028d383684a01d38878d
Andela_Vanesa <33512792+AndelaVanesa@users.noreply.github.com>
2018-02-19 18:18:47 +0100

    Add files via upload

---
+++ Osnove programiranja/filip/treci.py
@@ -0,0 +1,9 @@
+m = int(input('Unesite m: '))
+
+if m == 0:
+    m = int(input('m mora biti različit od 0. Ponovite unos: '))
+s = 0
+
+for i in range (1,m):
+    s += i/(i+1)
+print('{:.2f}'.format(s))
